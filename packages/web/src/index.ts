d83062c6482b0b3f163f6479b19722d0e15067f3
alexandersadchikov <alexandersadchikov@gologinapp.com>
2024-07-10 12:57:54 +0300

    #xo: (feat) added xo linter

--- packages/web/src/index.ts
+++ packages/web/src/index.ts
@@ -1,7 +1,4 @@
-import { FPClient } from './FPClient';
-
 export type { Descriptors } from './client-descriptors/index';
 export type { DescriptorsRes, FPClientInterface } from './types/index';
 
-export { FPClient };
-export default FPClient;
+export { FPClient, FPClient as default } from './FPClient';
