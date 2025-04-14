135ba9756da37f10bc0fe45284ad3d7ca1b814a0
alexandersadchikov <alexandersadchikov@gologinapp.com>
2025-04-14 14:42:01 +0300

    #errors-block: (feat) added errors block

---
+++ example/src/lib/components/ErrorInfo.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import type { Instance } from 'tippy.js';
+  import { tooltip } from '../utils/tippy';
+
+  export let error: { code: string; message: string };
+
+  let tooltipInstance: Instance | null = null;
+
+  async function copyToClipboard() {
+    try {
+      await navigator.clipboard.writeText(error.message);
+
+      tooltipInstance?.setContent('Copied!');
+      tooltipInstance?.show();
+
+      setTimeout(() => {
+        tooltipInstance?.setContent(error.message);
+      }, 600);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+</script>
+
+<button
+  type="button"
+  on:click={copyToClipboard}
+  use:tooltip={{
+    content: error.message,
+    placement: 'top',
+    onCreate(instance) {
+      tooltipInstance = instance;
+    }
+  }}
+>
+  {error.code}
+</button>
+
+<style>
+  button {
+    all: unset;
+    cursor: pointer;
+    color: hsl(348, 63%, 63%);
+  }
+</style>
---
+++ example/src/lib/components/Errors.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+  import Icon from '@iconify/svelte';
+  import ErrorInfo from './ErrorInfo.svelte';
+  import Accordion from './Accordion.svelte';
+  import type { GroupedError } from '../types';
+  
+  export let errorsData: GroupedError[] = [];
+
+  let sortOrder = 'desc';
+
+  $: errors = [...errorsData].sort((a, b) =>
+    sortOrder === 'desc'
+      ? b.code.localeCompare(a.code)
+      : a.code.localeCompare(b.code)
+  );
+
+  function toggleSortOrder() {
+    sortOrder = sortOrder === 'desc' ? 'asc' : 'desc';
+  }
+</script>
+
+<Accordion title="Errors" isOpen={true}>
+  <button on:click={toggleSortOrder} title={sortOrder === 'asc' ? 'Sort ascending' : 'Sort descending'}>
+    <Icon icon={sortOrder === 'asc' ? 'ph:sort-ascending' : 'ph:sort-descending'} width="18" height="18" />
+  </button>
+  <div class="errors">
+    {#each errors as { code, message, params }}
+      <div class="errors__item">
+        <span class="errors__item-name">{params.join(', ')}</span>
+        <ErrorInfo error={{ code, message }} />
+      </div>
+    {/each}
+  </div>
+</Accordion>
+
+<style>
+  .errors {
+    display: flex;
+    flex-direction: column;
+    gap: 0.6rem;
+    padding: 0.6rem 0;
+  }
+
+  .errors__item {
+    display: flex;
+    justify-content: space-between;
+    padding: 0.3rem 0.6rem;
+    background: hsl(0, 0%, 10%);
+    border-radius: 0.5rem;
+  }
+  
+  .errors__item-name {
+    flex: 1;
+    min-width: 0;
+    max-width: 68rem;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
+  button {
+    border: none;
+    border-radius: 0.3rem;
+    padding: 0.3rem 0.6rem;
+    cursor: pointer;
+    background: hsl(0, 0%, 33%);
+    transition: background-color 0.3s;
+  }
+
+  button:hover {
+    background-color: hsl(0, 0%, 20%);
+  }
+</style>
